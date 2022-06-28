f61f078ba2178aad8dd74f0035fa504221acb9dc
liz-izidoro <102263665+liz-izidoro@users.noreply.github.com>
2022-06-28 19:25:11 -0300

    Add files via upload

--- js/Cannon.js
+++ js/Cannon.js
@@ -1,24 +1,29 @@
 class Cannon {
-    constructor (x, y, width, height, angle) 
-    {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.angle = angle; 
-        this.cannonImg = loadImage("./assets/canon.png");
-        this.cannonBaseImg = loadImage("./assets/cannonBase.png");
+  constructor(x, y, width, height, angle) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.angle = angle;
+    this.cannon_image = loadImage("assets/canon.png");
+    this.cannon_base = loadImage("assets/cannonBase.png");
+  }
+  display() {
+    if (keyIsDown(RIGHT_ARROW) && this.angle<70  ) {
+      this.angle += 1;
     }
 
-    display()
-    {
-        //código para criar o topo do canhão
-        push();
-        imageMode(CENTER);
-        image(this.cannonImg, this.x, this.y, this.width, this.height);
-        pop();
-
-        //cria base do canhão
-        image(this.cannonBaseImg, 70, 20, 200, 200);
+    if (keyIsDown(LEFT_ARROW) && this.angle>-30 ) {
+      this.angle -= 1;
     }
+
+    push();
+    translate(this.x, this.y);
+    rotate(this.angle);
+    imageMode(CENTER);
+    image(this.cannon_image, 0, 0, this.width, this.height);
+    pop();
+    image(this.cannon_base, 70, 20, 200, 200);
+    noFill();
+  }
 }
